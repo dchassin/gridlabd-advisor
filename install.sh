5b6a25f61d4e0d321fb484024a322f7468d52c94
David P. Chassin <david.chassin@me.com>
2022-12-21 11:06:26 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python3 -m pip install -q openai
+python3 -m pip install -qq openai
 mkdir -p $HOME/.openai
-curl -sL https://raw.githubusercontent.com/dchassin/gridlabd-advisor/main/advisor.py -o $(gridlabd --version=install)/share/gridlabd/advisor.py || echo "ERROR: install failed" > /dev/stderr
+curl -sL https://raw.githubusercontent.com/dchassin/gridlabd-advisor/main/advisor.py -o $(gridlabd --version=install)/share/gridlabd/advisor.py && echo "Done. Use 'gridlabd advisor help' for more information." || echo "ERROR: install failed" > /dev/stderr
